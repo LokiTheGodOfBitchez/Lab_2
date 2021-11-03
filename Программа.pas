5b3c294f31640158f3980a6cac6b2fa5d01648c2
LokiTheGodOfBitchez <sofyachernova2002@gmail.com>
2021-11-03 23:07:59 +0300

    Код

---
+++ Программа.pas
@@ -0,0 +1,22 @@
+﻿program abcd;
+var
+a,b,c,x1,x2,D:real;
+begin
+Writeln('Введите коэффициенты квадратного уравнения');
+Writeln;
+Write('a = ');
+readln(a);
+Write('b = ');
+readln(b);
+Write('c = ');
+readln(c);
+D:=b*b-4*a*c;
+if D<0 then writeln('Действительных корней нет')
+else
+begin
+x1:=(-b+sqrt(D))/(2*a);
+x1:=(-b-sqrt(D))/(2*a);
+Writeln('x1 = ',x1);
+Writeln('x2 = ',x2);
+end;
+end.
